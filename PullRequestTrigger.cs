9aa0b56d3066fcf66ccd348c54e5df8f07d1ce45
nicruo <nicruo@gmail.com>
2020-05-06 15:26:36 +0100

    First code commit;

---
+++ PullRequestTrigger.cs
@@ -0,0 +1,138 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DXS.RebaseChecker
+{
+    public static class PullRequestTrigger
+    {
+        [FunctionName("PullRequestTrigger")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {                
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            await PostStatusOnPullRequest(data.resourceContainers.project.baseUrl.ToString(),  data.resourceContainers.project.id.ToString(), data.resource.repository.id.ToString(), data.resource.targetRefName.ToString());
+
+            return new OkObjectResult("");
+        }
+
+        private static async Task PostStatusOnPullRequest(string baseUrl, string projectName, string repositoryName, string targetRefName)
+        {
+            string baseServiceUrl = string.Format("{0}{1}/_apis/git/repositories/{2}",
+            baseUrl,
+            projectName,
+            repositoryName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(
+                        ASCIIEncoding.ASCII.GetBytes(
+                        string.Format("{0}:{1}", "", Environment.GetEnvironmentVariable("pat")))));
+
+                string truelastMergeTargetCommitId = await GetCommitIdFromRefNameAsync(baseServiceUrl, targetRefName, client);
+
+                dynamic pullRequests = await GetPullRequestByTargetRefNameAsync(baseServiceUrl, targetRefName, client);
+
+                if(pullRequests != null)
+                {
+                    foreach(var dataPullRequest in pullRequests)
+                    {
+                        int pullRequestId = (int)dataPullRequest.pullRequestId;
+                        string sourceRefName = dataPullRequest.sourceRefName.ToString();
+
+                        string truelastMergeSourceCommitId = await GetCommitIdFromRefNameAsync(baseServiceUrl, sourceRefName, client);
+
+                        string behindCount = await GetBehindCountAsync(baseServiceUrl, truelastMergeSourceCommitId, truelastMergeTargetCommitId, client);
+                        
+                        string serializedStatus = ComputePullRequestStatus(behindCount);
+
+                        await UpdatePullRequestStatusAsync(baseServiceUrl, pullRequestId, client, serializedStatus);
+                    }
+                }
+            }
+        }
+
+        private static async Task<dynamic> GetPullRequestByTargetRefNameAsync(string baseServiceUrl, string targetRefName, HttpClient httpClient)
+        {
+            string requestUrl = string.Format("{0}/pullrequests?searchCriteria.targetRefName={1}&api-version=5.1",
+            baseServiceUrl,
+            targetRefName);
+            var responseMessage = await httpClient.GetAsync(requestUrl);
+            string content = await responseMessage.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(content);
+            int pullRequestsCount = (int)data.count;
+            if(pullRequestsCount > 0)
+            {
+                return data.value;
+            }
+            return null;
+        }
+
+        private static async Task<string> GetCommitIdFromRefNameAsync(string baseServiceUrl, string refName, HttpClient httpClient)
+        {
+            string requestUrl = string.Format("{0}/{1}",
+            baseServiceUrl,
+            refName);
+            var responseMessage = await httpClient.GetAsync(requestUrl);
+            string content = await responseMessage.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(content);
+            return data.value[0].objectId.ToString();
+        }
+
+        private static async Task<string> GetBehindCountAsync(string baseServiceUrl, string sourceCommitId, string targetCommitId, HttpClient httpClient)
+        {
+            string requestUrl = string.Format("{0}/diffs/commits?diffCommonCommit=false&baseVersionType=commit&baseVersion={1}&targetVersionType=commit&targetVersion={2}",
+            baseServiceUrl,
+            targetCommitId,
+            sourceCommitId
+            );
+            var responseMessage = await httpClient.GetAsync(requestUrl);
+            var content = await responseMessage.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(content);
+            return data.behindCount.ToString();
+        }
+
+        private static async Task UpdatePullRequestStatusAsync(string baseServiceUrl, int pullRequestId, HttpClient httpClient, string serializedStatus)
+        {
+            string requestUrl = string.Format("{0}/pullrequests/{1}/statuses?api-version=5.1-preview.1",
+            baseServiceUrl,
+            pullRequestId);
+            await httpClient.PostAsync(requestUrl, new StringContent(serializedStatus, Encoding.UTF8, "application/json"));
+        }
+
+        private static string ComputePullRequestStatus(string behindCount)
+        {
+            string state = "succeeded";
+            string description = "Rebased with target. " + DateTime.Now;
+
+            if (behindCount != "0")
+            {
+                state = "failed";
+                description = "Please rebase to target first. Behind: " + behindCount + ". " + DateTime.Now;
+            }
+
+            return JsonConvert.SerializeObject(
+                new
+                {
+                    State = state,
+                    Description = description,
+                    Context = new
+                    {
+                        Name = "rebasechecker",
+                        Genre = "checker"
+                    }
+                });
+        }
+    }
+}
